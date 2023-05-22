eeadad2219d599fe739d981a8024cfdb7ddf7cfe
abdullahi <abdullahi12351@gmail.com>
2023-05-22 16:13:05 +0200

    Added more data to the person details + added what known for that shows what movies and tv-shows the has been involved in + updated some css

--- src/components/personDetails/PersonDetails.tsx
+++ src/components/personDetails/PersonDetails.tsx
@@ -1,19 +1,44 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Person } from '../personDetails/Types.tsx';
+import {useEffect, useState} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import {Person, PersonMovieKnownFor, PersonSeriesKnownFor} from '../personDetails/Types.tsx';
 import './PersonDetails.css';
-import {getPersonDetails} from "./Service.tsx";
+import {getPersonDetails, getPersonMovieDetails, getPersonSeriesDetails} from "./Service.tsx";
+import defaultImages from "../../assets/images.png";
+import CardList from "../card-list/card-list.tsx";
 
 export function PersonDetails() {
-    const { id } = useParams<{ id: string }>();
+    const {id} = useParams<{ id: string }>();
     const personId = Number(id);
 
-    const [personDetails, setPerieDetails] = useState<Person | null>(null);
+    const [personDetails, setPersonsDetails] = useState<Person | null>(null);
+    const [personMovieDetails, setPersonMovieDetails] = useState<PersonMovieKnownFor | null>(null);
+    const [personSeriesDetails, setPersonSeriesDetails] = useState<PersonSeriesKnownFor | null>(null);
 
     useEffect(() => {
         getPersonDetails(personId)
             .then((data: Person | null) => {
-                setPerieDetails(data);
+                setPersonsDetails(data);
+            })
+            .catch((error: any) => {
+                console.error(error);
+            });
+    }, [personId]);
+
+    useEffect(() => {
+        getPersonMovieDetails(personId)
+            .then((data: PersonMovieKnownFor | null) => {
+                setPersonMovieDetails(data);
+            })
+            .catch((error: any) => {
+                console.error(error);
+            });
+    }, [personId]);
+
+
+    useEffect(() => {
+        getPersonSeriesDetails(personId)
+            .then((data: PersonSeriesKnownFor | null) => {
+                setPersonSeriesDetails(data);
             })
             .catch((error: any) => {
                 console.error(error);
@@ -27,11 +52,43 @@ export function PersonDetails() {
     return (
         <div>
             <div className="seriedetails">
-                <img src={`https://image.tmdb.org/t/p/w300${personDetails.profile_path}`} />
+                <img src={`https://image.tmdb.org/t/p/w300${personDetails.profile_path}`}/>
+                <br/>
+                <br/>
                 <h1>{personDetails.name}</h1>
-                <p> <strong> place_of_birth:</strong> {personDetails.place_of_birth}</p>
-                <p> <strong> birthday:</strong> {personDetails.birthday}</p>
-                <p> <strong> known for:</strong> {personDetails.known_for_department}</p>
+                <p><strong> Biography:</strong> <br/>{personDetails.biography}</p>
+                <br/>
+                <p><strong> Known for: </strong>{personDetails.known_for_department}</p>
+                <br/>
+                <p><strong> Gender:</strong> {personDetails.gender === 1 ? 'Female' : "Male"}</p>
+                <br/>
+                <p><strong> Birthday:</strong> {personDetails.birthday}</p>
+                <br/>
+                <p><strong> Place of Birth:</strong> {personDetails.place_of_birth}</p>
+
+                <br/>
+                <br/>
+                <h2>Known For:</h2>
+                <div className='person_details_credit_cast'>
+                    {personMovieDetails?.cast.map((movie) => {
+                        const images = movie.poster_path === null ? defaultImages : `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
+                        return (
+                            <Link to={`/moviedetails/${movie.id}`}>
+                                <CardList category={movie.release_date} heading={movie.title} backgroundImage={images}/>
+                            </Link>
+                        )
+                    })}
+                    {personSeriesDetails?.cast.map((series) => {
+                        const images = series.poster_path === null ? defaultImages : `https://image.tmdb.org/t/p/w300${series.poster_path}`;
+                        return (
+                            <Link to={`/seriedetails/${series.id}`}>
+                                <CardList category={series.first_air_date} heading={series.name}
+                                          backgroundImage={images}/>
+                            </Link>
+
+                        )
+                    })}
+                </div>
 
             </div>
         </div>
--- src/components/personDetails/Types.tsx
+++ src/components/personDetails/Types.tsx
@@ -6,5 +6,35 @@ export interface Person {
     name: string;
     place_of_birth: string;
     profile_path: string | null;
+    gender:number;
+    biography:string;
 }
+
+interface Movie {
+    id: number;
+    poster_path: string;
+    title: string;
+    release_date: string;
+    vote_average: number;
+}
+
+interface TvSeries {
+    id: number;
+    poster_path: string;
+    name: string;
+    first_air_date: string;
+    vote_average: number;
+}
+export interface PersonMovieKnownFor {
+    cast:[Movie]
+}
+
+export interface PersonSeriesKnownFor {
+    cast:[TvSeries]
+}
+
+
+
+
+
 
