3d58f8aec23a9d021a1551dd7cf06ec1f3caf86a
lennartsargezian <Lennart_1997@hotmail.com>
2023-05-29 21:44:48 +0200

    tilføjet test til popular movies

--- src/components/Movies/Movies.test.ts
+++ src/components/Movies/Movies.test.ts
@@ -19,17 +19,4 @@ test('list of popular movies', async () => {
         { id: 238, title: 'The Godfather' },
         { id: 123, title: 'Inception' },
     ]);
-    expect(fetch).toHaveBeenCalledWith(
-        'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1',
-        {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.' +
-                    'eyJhdWQiOiJlMjI1OTY2ZTA2NDcwOGZlY2U0MjU' +
-                    '2OTAyYjZmNjJkNSIsInN1YiI6IjY0NTc0YjAzZmU' +
-                    'wNzdhMDEzOThiZDQyNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n-FpTuOAfnnvgFwjTBs_7a0ak9iO9OG8SBSxH0nuPos',
-            },
-        }
-    );
 });
